a7b2599d3683ba816ba12eaa1a6875a9c09c7f46
Marinelic <75620592+Marinelic@users.noreply.github.com>
2024-10-26 21:53:15 +0200

    First Project With React

---
+++ src/containers/whatProject1/WhatProject1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatProject1.css';
+
+const WhatProject1 = () => {
+  return (
+    <div>
+      WhatProject1
+    </div>
+  )
+}
+
+export default WhatProject1
