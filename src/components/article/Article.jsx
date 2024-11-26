8ddf2ae80bc0df7305f36358417010e00d6142c9
Marinelic <75620592+Marinelic@users.noreply.github.com>
2024-11-26 17:14:56 +0100

    Finishing Section Blog and Footer

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, text}) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+          <img src={imgUrl} alt="blog" />
+      </div>
+
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+
+        <p>Read Fill Article</p>
+      </div>
     </div>
   )
 }
