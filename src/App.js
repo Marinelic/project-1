1f37a26009b7b234f31b9192a66c941927c56b8f
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-02-01 22:43:39 +0100

    Twitter Clone Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Footer, Blog, Possibility, Features, WhatProject1, Header } from './containers';
-import { CTA, Brand, Navbar } from './components';
 import './App.css';
+import Sidebar from './Sidebar';
 
-
-const root = createRoot(document.getElementById('root'));
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-        <Brand />
-        <WhatProject1 />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Blog />
-        <Footer />
+    <div className='app'>
+     <h1>Twitter Clone Page</h1>
+
+     <Sidebar />
+
+
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/* Twitter icon */}
+      <TwitterIcon />
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button Tweet */}
+    </div>
+  )
+}
+
+export default Sidebar;
